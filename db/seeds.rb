bc47a6536e9478e24e77f22a46fc1e3088c1ee16
Bogdanov Anton <kortirso@gmail.com>
2025-02-02 16:11:45 +0300

    update list of dnd5 spells from seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -2,41 +2,20 @@
 
 require 'csv'
 
-# spells_file = File.read(Rails.root.join('db/data/spells.json'))
-# spells = JSON.parse(spells_file)
-# spells.each do |spell|
-#   ::Spell.create!({ type: 'Dnd5::Spell', slug: spell['slug'], name: spell['name'], data: spell.except('name', 'slug') })
-# end
-
-# https://github.com/Etignis/DnD_SpellList_eng_rus/blob/master/spells/allSpells.js
-spells_file = File.read(Rails.root.join('db/data/full_spells.json'))
-selected_spells = JSON.parse(spells_file).select { |spell| spell['en']['source'].include?('PHB') || spell['en']['source'].include?('XGTE') || spell['en']['source'].include?('TCoE') }
-selected_spells = selected_spells.sort_by { |spell| spell['en']['level'].to_i }
-selected_spells = selected_spells.map do |spell|
-  [
-    spell.dig('en', 'level').to_s,
-    spell.dig('en', 'name'),
-    spell.dig('ru', 'name'),
-    spell.dig('en', 'school').downcase.strip,
-    spell.dig('en', 'source')
-  ]
-end
-File.write(Rails.root.join('db/data/dnd5_spells.csv'), selected_spells.map(&:to_csv).join)
-
-spells = CSV.parse(File.read(Rails.root.join('db/data/dnd5_spells.csv')), headers: false)
+spells = CSV.parse(File.read(Rails.root.join('db/data/dnd5_spells.csv')), headers: false, col_sep: ';')
 spells.map! do |spell|
   {
     type: 'Dnd5::Spell',
-    slug: spell[1].to_underscore,
+    slug: spell[1],
     name: {
-      en: spell[1],
-      ru: spell[2]
+      en: spell[2],
+      ru: spell[3]
     },
     data: {
       level: spell[0].to_i,
-      school: spell[3],
-      available_for: [],
-      source: spell[4]
+      school: spell[4],
+      available_for: spell[6].split(','),
+      source: spell[5]
     }
   }
 end
