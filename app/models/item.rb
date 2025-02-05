83fc171d4fc9552d6c435b69e04e56c9d031ae3a
Bogdanov Anton <kortirso@gmail.com>
2025-02-05 22:27:31 +0300

    fix model has many relation

--- app/models/item.rb
+++ app/models/item.rb
@@ -3,5 +3,5 @@
 class Item < ApplicationRecord
   scope :dnd5, -> { where(type: 'Dnd5::Item') }
 
-  has_many :character_items, dependent: :destroy
+  has_many :character_items, class_name: 'Character::Item', dependent: :destroy
 end
