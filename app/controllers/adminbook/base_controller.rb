0af8b665e60b7673c5f0852d0f548d2c4f68ca2f
Bogdanov Anton <kortirso@gmail.com>
2025-02-14 10:40:52 +0300

    fix http_auth

--- app/controllers/adminbook/base_controller.rb
+++ app/controllers/adminbook/base_controller.rb
@@ -2,8 +2,8 @@
 
 module Adminbook
   class BaseController < ApplicationController
-    http_basic_authenticate_with name: Rails.application.credentials.adminbook&.fetch(:username),
-                                 password: Rails.application.credentials.adminbook&.fetch(:password),
+    http_basic_authenticate_with name: Rails.application.credentials.adminbook&.fetch(:username, '') || '',
+                                 password: Rails.application.credentials.adminbook&.fetch(:password, '') || '',
                                  if: -> { Rails.env.production? }
 
     skip_before_action :authenticate
