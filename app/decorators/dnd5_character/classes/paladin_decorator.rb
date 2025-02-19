3fed12c83212d852c6c4ae9a0d42b656d5b87951
Bogdanov Anton <kortirso@gmail.com>
2025-02-19 22:25:11 +0300

    add D&D 5 character features for palading and cleric

--- app/decorators/dnd5_character/classes/paladin_decorator.rb
+++ app/decorators/dnd5_character/classes/paladin_decorator.rb
@@ -5,6 +5,7 @@ module Dnd5Character
     class PaladinDecorator
       WEAPON_CORE = ['light weapon', 'martial weapon'].freeze
       ARMOR = ['light armor', 'medium armor', 'heavy armor', 'shield'].freeze
+      IMMUNITIES = %w[disease].freeze
 
       def decorate_fresh_character(result:)
         result[:weapon_core_skills] = result[:weapon_core_skills].concat(WEAPON_CORE).uniq
@@ -18,6 +19,9 @@ module Dnd5Character
       def decorate_character_abilities(result:, class_level:)
         result[:class_save_dc] = %i[wis cha] if result[:main_class] == 'paladin'
         result[:hit_dice][10] += class_level
+
+        result[:immunities] = result[:immunities].concat(IMMUNITIES).uniq
+        result[:combat][:attacks_per_action] = 2 if class_level >= 5 # Extra Attack, 5 level
 
         result
       end
