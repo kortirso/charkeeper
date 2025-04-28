8e1574cbe0103157b1f77b136b9b838624e929a7
Bogdanov Anton <kortirso@gmail.com>
2025-04-28 14:14:49 +0300

    add custom font

--- app/javascript/applications/WebTelegram/components/molecules/PageHeader.jsx
+++ app/javascript/applications/WebTelegram/components/molecules/PageHeader.jsx
@@ -4,13 +4,13 @@ export const PageHeader = (props) => {
   const safeChildren = children(() => props.children);
 
   return (
-    <div class="w-full flex justify-between items-center p-4 bg-white border-b border-gray-200">
+    <div class="w-full flex justify-between items-center px-4 py-3 bg-white border-b border-gray-200">
       <Show when={props.leftContent || props.rightContent}>
         <div class="w-12">
           {props.leftContent}
         </div>
       </Show>
-      <div class="flex-1 flex flex-col items-center">
+      <div class="flex-1 flex flex-col items-center text-2xl">
         {safeChildren()}
       </div>
       <Show when={props.leftContent || props.rightContent}>
--- config/tailwind.config.js
+++ config/tailwind.config.js
@@ -1,4 +1,4 @@
-// const defaultTheme = require('tailwindcss/defaultTheme');
+const { fontFamily } = require('tailwindcss/defaultTheme');
 
 module.exports = {
   darkMode: 'selector',
@@ -12,5 +12,13 @@ module.exports = {
     // require('@tailwindcss/forms'),
     // require('@tailwindcss/typography'),
     // require('@tailwindcss/container-queries'),
-  ]
+  ],
+  theme: {
+    fontFamily: {
+      'sans': ['CascadiaMono-SemiLight', 'sans-serif', ...fontFamily.sans],
+      'cascadia-light': ['CascadiaMono-Light', ...fontFamily.sans],
+      'cascadia': ['CascadiaMono-SemiLight', ...fontFamily.sans],
+      'cascadia-bold': ['CascadiaMono', ...fontFamily.sans]
+    }
+  },
 }
