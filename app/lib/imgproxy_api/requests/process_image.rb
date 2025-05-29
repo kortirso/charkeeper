ad4bedbb6a367d3abf3b235c81ea6d21f46a824f
Bogdanov Anton <kortirso@gmail.com>
2025-05-29 21:27:49 +0300

    add secure url for imgproxy

--- app/lib/imgproxy_api/requests/process_image.rb
+++ app/lib/imgproxy_api/requests/process_image.rb
@@ -1,14 +1,30 @@
 # frozen_string_literal: true
 
 require 'base64'
+require 'openssl'
 
 module ImgproxyApi
   module Requests
     module ProcessImage
       def process_image(url:, extension:, processing_options: [])
+        process_url = "/#{processing_options.join('/')}/#{Base64.urlsafe_encode64(url, padding: false)}.#{extension}"
+        digest = credentials.nil? ? 'unsafe' : generate_digest(process_url)
         get(
-          path: "unsafe/#{processing_options.join('/')}/#{Base64.urlsafe_encode64(url, padding: false)}.#{extension}"
+          path: "#{digest}#{process_url}"
         )
+      end
+
+      private
+
+      def generate_digest(process_url)
+        Base64.urlsafe_encode64(
+          OpenSSL::HMAC.digest('sha256', credentials.secret, "#{credentials.salt}#{process_url}"),
+          padding: false
+        )
+      end
+
+      def credentials
+        Rails.application.credentials.dig(Rails.env.to_sym, :imgproxy)
       end
     end
   end
