e68f565fde68a0884019deea7d4ba952950605a5
Bogdanov Anton <kortirso@gmail.com>
2025-06-15 17:43:51 +0300

    fix 2024 presenter

--- app/decorators/dnd2024_character/classes/fighter_decorator.rb
+++ app/decorators/dnd2024_character/classes/fighter_decorator.rb
@@ -3,7 +3,7 @@
 module Dnd2024Character
   module Classes
     class FighterDecorator < ApplicationDecorator
-      CLASS_SAVE_DC = %w[str com].freeze
+      CLASS_SAVE_DC = %w[str con].freeze
 
       def class_save_dc
         @class_save_dc ||= main_class == 'fighter' ? CLASS_SAVE_DC : __getobj__.class_save_dc
