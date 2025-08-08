fe2542f761b5819a49cd42b506749021553024ea
Bogdanov Anton <kortirso@gmail.com>
2025-08-08 18:43:49 +0300

    update cors

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -4,5 +4,6 @@ Rails.application.config.middleware.insert_before 0, Rack::Cors do
   allow do
     origins '*'
     resource '/frontend/*', headers: :any, methods: %i[get post put patch delete options head], credentials: false
+    resource '/characters/*', headers: :any, methods: %i[get], credentials: false
   end
 end
