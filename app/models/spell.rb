5196733dbd6f7b2b0e5d941330d43f704003a4f5
Bogdanov Anton <kortirso@gmail.com>
2025-10-16 13:19:59 +0300

    remove daggerheart spells class

--- app/models/spell.rb
+++ app/models/spell.rb
@@ -5,5 +5,4 @@ class Spell < ApplicationRecord
 
   scope :dnd5, -> { where(type: 'Dnd5::Spell') }
   scope :dnd2024, -> { where(type: 'Dnd2024::Spell') }
-  scope :daggerheart, -> { where(type: 'Daggerheart::Spell') }
 end
