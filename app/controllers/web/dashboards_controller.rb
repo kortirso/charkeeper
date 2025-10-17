5ea9ef126f2377e035f5bda08644de16d39edfec
Bogdanov Anton <kortirso@gmail.com>
2025-10-17 11:54:29 +0300

    IS-352 attaching oauth accounts to existing account (#355)
    * IS-352 attaching oauth accounts to existing account
    
    * rendering identities in app, removing identities

--- app/controllers/web/dashboards_controller.rb
+++ app/controllers/web/dashboards_controller.rb
@@ -2,10 +2,23 @@
 
 module Web
   class DashboardsController < Web::BaseController
+    include Authkeeper::ApplicationHelper
+
     layout 'charkeeper_app'
 
     def show
       @access_token = cookies[Authkeeper.configuration.access_token_name]
+      @identities = current_user.identities.hashable_pluck(:id, :uid, :provider)
+      @oauth_links = {
+        google: omniauth_link(:google),
+        discord: omniauth_link(:discord)
+      }
+      @oauth_credentials = {
+        telegram: {
+          bot_name: Rails.application.credentials.dig(Rails.env.to_sym, :oauth, :telegram, :bot_name),
+          redirect_url: Rails.application.credentials.dig(Rails.env.to_sym, :oauth, :telegram, :redirect_url)
+        }
+      }
     end
   end
 end
