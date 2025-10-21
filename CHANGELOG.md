505fee836b70de4c63681e68016f65527c565529
Bogdanov Anton <kortirso@gmail.com>
2025-10-21 16:32:09 +0300

    IS-380 modified selecting domain cards

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -7,6 +7,7 @@ and this project adheres to [Semantic Versioning](http://semver.org/spec/v2.0.0.
 ## Unreleased
 ### Modified
 - case-insensitive items search with auto toggling
+- selecting domain cards
 
 ### Fixed
 - refreshing bonuses list
--- app/commands/characters_context/daggerheart/add_spell_command.rb
+++ app/commands/characters_context/daggerheart/add_spell_command.rb
@@ -14,13 +14,19 @@ module CharactersContext
 
       private
 
+      def do_prepare(input)
+        active_spells =
+          input[:character].feats.joins(:feat).where(ready_to_use: true).where(feats: { origin: 7 }).count
+        input[:ready_to_use] = active_spells < 5
+      end
+
       def do_persist(input)
         result = ::Daggerheart::Character::Feat.create!(
           character: input[:character],
           feat: input[:spell],
           used_count: 0,
           limit_refresh: input[:spell].limit_refresh,
-          ready_to_use: false
+          ready_to_use: input[:ready_to_use]
         )
 
         { result: result }
