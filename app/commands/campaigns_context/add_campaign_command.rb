0674cfc19d23672f2b72b1f09dd77655e0bdde70
Bogdanov Anton <kortirso@gmail.com>
2026-02-08 11:39:22 +0300

    add Fate to campaigns

--- app/commands/campaigns_context/add_campaign_command.rb
+++ app/commands/campaigns_context/add_campaign_command.rb
@@ -5,7 +5,7 @@ module CampaignsContext
     use_contract do
       config.messages.namespace = :campaign
 
-      Providers = Dry::Types['strict.string'].enum('dnd5', 'dnd2024', 'daggerheart', 'pathfinder2', 'dc20')
+      Providers = Dry::Types['strict.string'].enum('dnd5', 'dnd2024', 'daggerheart', 'pathfinder2', 'dc20', 'fate')
 
       params do
         required(:user).filled(type?: User)
