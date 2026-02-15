68c0376afcc6016ebe6a2e3256e6f5fb85142c4d
Bogdanov Anton <kortirso@gmail.com>
2026-02-15 14:05:29 +0300

    IS-617 added character info block

--- app/serializers/dnd2024/character_serializer.rb
+++ app/serializers/dnd2024/character_serializer.rb
@@ -9,10 +9,10 @@ module Dnd2024
                :attacks, :selected_features, :resistances, :death_saving_throws, :health, :spent_hit_dice,
                :spent_spell_slots, :coins, :load, :languages, :tools, :music, :weapon_core_skills, :weapon_skills,
                :armor_proficiency, :spell_classes, :spells_slots, :static_spells, :created_at, :avatar, :selected_feats,
-               :darkvision, :modified_abilities, :available_spell_level, :formatted_static_spells, :conditions,
+               :darkvision, :modified_abilities, :available_spell_level, :formatted_static_spells, :conditions, :background,
                :selected_beastforms, :beastform, :weapon_mastery, :speeds, :money, :guide_step, :ability_boosts,
                :any_skill_boosts, :skill_boosts, :skill_boosts_list, :heroic_inspiration, :bardic_inspiration, :selected_talents,
-               :leveling_ability_boosts, :leveling_ability_boosts_list, :available_talents, :skills, :exhaustion
+               :leveling_ability_boosts, :leveling_ability_boosts_list, :available_talents, :skills, :exhaustion, :alignment
 
     delegate :features, :id, :name, :level, :species, :legacy, :main_class, :classes, :subclasses, :abilities, :skills,
              :modifiers, :save_dc, :proficiency_bonus, :hit_dice, :armor_class, :initiative, :speed, :attacks_per_action,
@@ -24,7 +24,7 @@ module Dnd2024
     delegate :created_at, :data, to: :object
     delegate :guide_step, :ability_boosts, :any_skill_boosts, :skill_boosts, :skill_boosts_list, :heroic_inspiration,
              :bardic_inspiration, :selected_talents, :leveling_ability_boosts, :leveling_ability_boosts_list, :exhaustion,
-             to: :data
+             :alignment, :background, to: :data
 
     def provider
       'dnd2024'
