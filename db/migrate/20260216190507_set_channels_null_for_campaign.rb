89712c2e0279f2717424de72fcd1dd08aeda3a7a
Bogdanov Anton <kortirso@gmail.com>
2026-02-17 12:23:29 +0300

    remove migration

--- db/migrate/20260216190507_set_channels_null_for_campaign.rb
+++
@@ -1,9 +0,0 @@
-class SetChannelsNullForCampaign < ActiveRecord::Migration[8.1]
-  def up
-    safety_assured { change_column_null :channels, :campaign_id, true }
-  end
-
-  def down
-    safety_assured { change_column_null :channels, :campaign_id, false }
-  end
-end
