168672028c732bf0442e0acb9a76938a6c778911
Bogdanov Anton <kortirso@gmail.com>
2026-02-28 08:20:36 +0300

    release 0.4.10

--- app/lib/config.rb
+++ app/lib/config.rb
@@ -3,8 +3,8 @@
 module Config
   extend self
 
-  def data(provider, key, version: '0.4.9')
-    Rails.cache.fetch("#{provider}/#{key}/#{version}") { load_data(provider, key) }
+  def data(provider, key, version: '0.4.10')
+    Rails.cache.fetch("#{provider}/#{key}/#{version}", expires_in: 3.days) { load_data(provider, key) }
   end
 
   private
--- app/lib/platform_config.rb
+++ app/lib/platform_config.rb
@@ -3,8 +3,8 @@
 module PlatformConfig
   extend self
 
-  def data(provider, version: '0.4.9')
-    Rails.cache.fetch("#{provider}/#{version}") { load_data(provider) }
+  def data(provider, version: '0.4.10')
+    Rails.cache.fetch("#{provider}/#{version}", expires_in: 3.days) { load_data(provider) }
   end
 
   private
