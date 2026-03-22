0a2f5dfb7310d99411f972bf833964690fcffb1d
Bogdanov Anton <kortirso@gmail.com>
2026-03-22 18:28:30 +0300

    update seeds

--- app/helpers/translate_helper.rb
+++ app/helpers/translate_helper.rb
@@ -7,10 +7,10 @@ module TranslateHelper
     return unless hash.is_a?(Hash)
 
     hash = hash.with_indifferent_access
-    return hash[I18n.locale] if hash.key?(I18n.locale)
+    return hash[I18n.locale] if hash[I18n.locale].present?
 
     fallback_locale = I18N_FALLBACKS[I18n.locale.to_s]
-    return hash[fallback_locale] if fallback_locale && hash.key?(fallback_locale)
+    return hash[fallback_locale] if fallback_locale && hash[fallback_locale].present?
 
     hash['en'] || hash[:en]
   end
