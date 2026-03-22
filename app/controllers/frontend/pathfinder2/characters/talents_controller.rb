7e2a1f2622c5cb9c7a77453dcbe6d0819570e7e4
Bogdanov Anton <kortirso@gmail.com>
2026-03-22 16:10:43 +0300

    update cache key

--- app/controllers/frontend/pathfinder2/characters/talents_controller.rb
+++ app/controllers/frontend/pathfinder2/characters/talents_controller.rb
@@ -30,7 +30,7 @@ module Frontend
         private
 
         def cache_options
-          { key: "feats/#{params[:provider]}/#{I18n.locale}/v1", expires_in: 1.day }
+          { key: "feats/#{params[:provider]}/#{I18n.locale}/v2", expires_in: 1.day }
         end
 
         def find_character
@@ -52,7 +52,7 @@ module Frontend
         end
 
         def tags
-          check_cache_value({ key: "feat_tags/#{params[:provider]}/#{I18n.locale}/v1", expires_in: 1.day }) do
+          check_cache_value({ key: "feat_tags/#{params[:provider]}/#{I18n.locale}/v2", expires_in: 1.day }) do
             ::Pathfinder2::Feat.pluck(:origin_values).flatten.uniq.index_with do |item|
               I18n.t("tags.pathfinder.general.#{item}")
             end
