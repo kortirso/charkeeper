0ae5910e2fd49bac4e1dc0c6757e8c7073eb8e16
Bogdanov Anton <kortirso@gmail.com>
2026-03-23 21:14:40 +0300

    IS-583 added loot tables for DH characters

--- app/platforms/daggerheart/item.rb
+++ app/platforms/daggerheart/item.rb
@@ -8,6 +8,20 @@ end
 
 module Daggerheart
   class Item < Item
+    LOOT_TABLES = %w[item consumables].freeze
+
     attribute :data, Daggerheart::ItemData.to_type
+
+    def self.loot(type:, dices: 1)
+      dices = 6 unless dices.to_i.between?(1, 6)
+      type = 'item' if LOOT_TABLES.exclude?(type)
+
+      find_by(
+        slug: Config.data('daggerheart', 'loot_tables').dig(
+          type,
+          Charkeeper::Container.resolve('roll').call(dice: "#{dices}d12")
+        )
+      )
+    end
   end
 end
