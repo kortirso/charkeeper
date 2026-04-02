9bbdcad0b1c256b2bc2784c7a4ff4fa508a2af50
Bogdanov Anton <kortirso@gmail.com>
2026-04-02 15:37:29 +0300

    update background feats

--- db/seeds_prod.rb
+++ db/seeds_prod.rb
@@ -1,4 +1,4 @@
-Dir[File.join(Rails.root.join('db/data_prod/pathfinder2/spells0.json'))].each do |filename|
+Dir[File.join(Rails.root.join('db/data_prod/pathfinder2/*.json'))].each do |filename|
   puts "seeding - #{filename}"
   JSON.parse(File.read(filename)).each do |item|
     feat = ::Pathfinder2::Feat.find_by(slug: item['slug'])
