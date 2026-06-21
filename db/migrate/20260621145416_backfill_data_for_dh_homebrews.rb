9597060a4a3785df60849a75acac737c748cc55c
Bogdanov Anton <kortirso@gmail.com>
2026-06-21 19:52:26 +0300

    update seeds

--- db/migrate/20260621145416_backfill_data_for_dh_homebrews.rb
+++ db/migrate/20260621145416_backfill_data_for_dh_homebrews.rb
@@ -1,31 +1,24 @@
 class BackfillDataForDhHomebrews < ActiveRecord::Migration[8.1]
   def up
-    Homebrew::Book::Item.where(itemable_type: 'Homebrew').each do |item|
+    Homebrew::Book::Item.where(itemable_type: 'Daggerheart::Homebrew::Subclass').each do |item|
       new_item = ::Daggerheart::Homebrews::Subclass.find_by(id: item.itemable_id)
       next unless new_item
 
       item.update(itemable_id: new_item.id, itemable_type: 'Daggerheart::Homebrews::Subclass')
     end
 
-    Homebrew::Book::Item.where(itemable_type: 'Homebrew').each do |item|
+    Homebrew::Book::Item.where(itemable_type: 'Daggerheart::Homebrew::Race').each do |item|
       new_item = ::Daggerheart::Homebrews::Ancestry.find_by(id: item.itemable_id)
       next unless new_item
 
       item.update(itemable_id: new_item.id, itemable_type: 'Daggerheart::Homebrews::Ancestry')
     end
 
-    Homebrew::Book::Item.where(itemable_type: 'Homebrew').each do |item|
+    Homebrew::Book::Item.where(itemable_type: 'Daggerheart::Homebrew::Community').each do |item|
       new_item = ::Daggerheart::Homebrews::Community.find_by(id: item.itemable_id)
       next unless new_item
 
       item.update(itemable_id: new_item.id, itemable_type: 'Daggerheart::Homebrews::Community')
-    end
-
-    Homebrew::Book::Item.where(itemable_type: 'Homebrew').each do |item|
-      new_item = ::Daggerheart::Homebrews::Domain.find_by(id: item.itemable_id)
-      next unless new_item
-
-      item.update(itemable_id: new_item.id, itemable_type: 'Daggerheart::Homebrews::Domain')
     end
   end
 
